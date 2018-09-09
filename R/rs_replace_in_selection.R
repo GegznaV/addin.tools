1155b3e53cb0021f4c2e0e0f9f32e6b4e3be38fd
Vilmantas Gegzna <GegznaV@gmail.com>
2018-09-09 04:24:22 +0300

    Option "keep_selected" added

--- R/rs_replace_in_selection.R
+++ R/rs_replace_in_selection.R
@@ -4,6 +4,7 @@
 #'
 #' @param pattern (character) A pattern of text to be replaced.
 #' @param replacement (character) The replacement text.
+#' @param keep_selected (logical) Flag indicating if the selection should be selected after add-in was applied.
 #' @param fixed (logical)
 #'        If \code{TRUE}, the pattern is a fixed expression.
 #'        If \code{FALSE}, the pattern is a regular expression.
@@ -12,10 +13,8 @@
 #'
 #' @seealso \code{\link[base]{gsub}}
 #' @export
-rs_replace_in_selection <- function(pattern,
-                                    replacement,
-                                    fixed = TRUE,
-                                    context = rs_get_context()) {
+rs_replace_in_selection <- function(pattern, replacement, fixed = TRUE,
+                                    keep_selected = TRUE, context = rs_get_context()) {
 
     sel <- context$selection[[1]]
     old_text <- sel$text
@@ -29,6 +28,20 @@ rs_replace_in_selection <- function(pattern,
     rstudioapi::modifyRange(location = sel$range,
                             text = as.character(new_text),
                             id = context$id)
+
+    if (keep_selected) {
+        new_range <- sel$range
+
+        if (new_range$start[["row"]] == new_range$end[["row"]]) {
+            # If selection spans only one row, the beggining of the selection is chosen.
+            end_ind <- new_range$start[["column"]] + nchar(new_text)
+        } else {
+            # If selection spans several rows, the beggining of the last row is chosen.
+            end_ind <- nchar(stringr::str_extract(new_text, "(?<!\n).*?$")) + 2
+        }
+        new_range$end[["column"]] <- end_ind
+        rstudioapi::setSelectionRanges(new_range)
+    }
 
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
