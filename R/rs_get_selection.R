2d371032f86cfc3077a13911894ce639964687d0
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-14 13:55:43 +0200

    Update R/rs_get_selection.R

--- R/rs_get_selection.R
+++ R/rs_get_selection.R
@@ -1,38 +1,50 @@
 #' Get length of selection
 #'
-#' Calculate number of characters in the first selection.
+#' Calculate number of characters in the each selection.
 #'
 #' @inheritParams rs_get_index
 #'
-#' @return Integer with number of characters in the first selection.
+#' @return An integer vector with number of characters in each selection.
 #' @export
-rs_get_selection_length <- function(context = rs_get_context()) {
-    nchar(rs_get_selection_text(context = context))
+rs_get_selection_length <- function(context = rs_get_context(), type = c("first", "all")) {
+    nchar(rs_get_selection_text(context = context, type = type))
 }
+
 rs_get_row_lengths <- function(row, end_row = NULL, context = rs_get_context()) {
-    nchar(rs_get_rows(context = context))
+    nchar(rs_get_rows(row = row, end_row = end_row, context = context))
 }
 
 #' Get selection text
 #'
-#' Get the text in the first selection.
+#' Get the text in either the first selection or all selections
 #'
 #' @inheritParams rs_get_index
 #'
-#' @return String.
+#' @return A character vector.
 #' @export
-rs_get_selection_text <- function(context = rs_get_context()) {
-    context$selection[[1]]$text
-}
+
+rs_get_selection_text <- function(context = rs_get_context(), type = c("first", "all")) {
+
+    type <- match.arg(type)
+    switch(type,
+           "first" = context$selection[[1]]$text,
+           "all"   = purrr::map_chr(context$selection, "text")
+    )
+ }
 
 #' Get range of selection
 #'
-#' Get the range of the first selection.
+#' Get the range of the first/each selection.
 #'
 #' @inheritParams rs_get_index
 #'
-#' @return "document_range" object.
+#' @return Either a "document_range" object or a list of those objects.
 #' @export
-rs_get_selection_range <- function(context = rs_get_context()) {
-    context$selection[[1]]$range
+rs_get_selection_range <- function(context = rs_get_context(), type = c("first", "all")) {
+
+    type <- match.arg(type)
+    switch(type,
+           "first" = context$selection[[1]]$range,            # returns range object
+           "all"   = purrr::map(context$selection, "range")   # returns a list of range objects
+    )
 }
