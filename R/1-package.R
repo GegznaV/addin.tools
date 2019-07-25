c0bcc4cedae3098ee24cde663ff9b687a2c70bd2
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-25 20:09:01 +0300

    Update 1-package.R

--- R/1-package.R
+++ R/1-package.R
@@ -1,11 +1,8 @@
-# [!!!]
-#
 # TODO:
 #
 # 1) make "insert" and "replace" functions to insert text if there are
 #    several selections (see function rs_get_selection_text() parameter `type`
 #    for details).
-
 
 #' @name addin.tools
 #' @docType package
@@ -21,7 +18,7 @@
 #' \code{URL:} \url{https://github.com/GegznaV/addin.tools} \cr
 #' \code{Bug reports and suggestions:} \url{https://github.com/GegznaV/addin.tools/issues} \cr
 #' \code{Author:} Vilmantas Gegzna
-
+#'
+#' @import rstudioapi
 
 NULL
-
