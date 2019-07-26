f469a025f571205d0250a0cc8885feed7836c423
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-26 17:40:30 +0300

    Update rs_get_ind.R

--- R/rs_get_ind.R
+++ R/rs_get_ind.R
@@ -10,49 +10,61 @@ NULL
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rs_get_index
 #' @export
-rs_get_first_selected_col_index <- function(context = rs_get_context()) {
-    context$selection[[1]]$range$start["column"]
+rs_get_selection_start_index <- function(selection = c("last", "first"),
+                                         context = rs_get_context()) {
+    selection <- match.arg(selection)
+    rs_get_selection_range(selection, context = context)$start
 }
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rs_get_index
 #' @export
-rs_get_last_selected_col_index <- function(context = rs_get_context()) {
-    context$selection[[1]]$range$end["column"]
+rs_get_selection_end_index <- function(selection = c("last", "first"),
+                                       context = rs_get_context()) {
+    selection <- match.arg(selection)
+    rs_get_selection_range(selection, context = context)$end
 }
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname rs_get_index
+#' @export
+rs_get_first_selected_col_index <- function(selection = c("first", "last"),
+                                            context = rs_get_context()) {
+    selection <- match.arg(selection)
+    rs_get_selection_start_index(selection, context = context)["column"]
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname rs_get_index
+#' @export
+rs_get_last_selected_col_index <- function(selection = c("last", "first"),
+                                           context = rs_get_context()) {
+    selection <- match.arg(selection)
+    rs_get_selection_end_index(selection, context = context)["column"]
+}
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rs_get_index
 #' @export
 rs_get_first_selected_row_index <- function(context = rs_get_context()) {
-    context$selection[[1]]$range$start["row"]
+    rs_get_selection_start_index(context = context)["row"]
 }
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rs_get_index
 #' @export
-rs_get_last_selected_row_index <- function(context = rs_get_context()) {
-    context$selection[[1]]$range$end["row"]
+rs_get_last_selected_row_index <- function(selection = c("last", "first"),
+                                           context = rs_get_context()) {
+    selection <- match.arg(selection)
+    rs_get_selection_range(selection, context = context)$end["row"]
 }
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rs_get_index
 #' @export
 rs_get_selected_row_indexes <- function(context = rs_get_context()) {
-    first <- context$selection[[1]]$range$start["row"]
-    last  <- context$selection[[1]]$range$end["row"]
+    first <- rs_get_selection_start_index("first", context = context)["row"]
+    last  <- rs_get_selection_end_index("last",    context = context)["row"]
     first:last
 }
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' @rdname rs_get_index
-#' @export
-rs_get_selection_start_index <- function(context = rs_get_context()) {
-    context$selection[[1]]$range$start
-}
-#' @rdname rs_get_index
-#' @export
-rs_get_selection_end_index <- function(context = rs_get_context()) {
-    context$selection[[1]]$range$end
-}
-
-
-
-
