e849d3de6621d9b19e407c0963079d3255f17fdf
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-26 20:47:21 +0300

    Update is_blank_line_needed_above.R

--- R/is_blank_line_needed_above.R
+++ R/is_blank_line_needed_above.R
@@ -9,12 +9,12 @@
 #'
 #' @export
 is_blank_line_needed_above <- function(context = rs_get_context()) {
-    row <- rs_get_first_selected_row_index(context)
+    row <- rs_get_first_selected_row_index(context = context)
 
     # Contents of row above the selection:
     txt <- context$contents[row - 1]
     # Remove spaces and check if string is empty:
-    cond <- stringi::stri_isempty(gsub("[[:space:]]", "", txt))
+    cond <- is_empty_or_spaces(txt)
     # If not empty (result: FALSE) and not the first row (result: logical(0)),
     # then an empty row needs to be added:
     isTRUE(!cond)
@@ -22,4 +22,8 @@ is_blank_line_needed_above <- function(context = rs_get_context()) {
 
 is_empty_str <- function(str) {
     nchar(str) == 0
+}
+
+is_empty_or_spaces <- function(str) {
+    is_empty_str(gsub("[[:space:]]", "", str))
 }
