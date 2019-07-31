f210686841197c6ba26cfc82dc556b1413ac7a39
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-31 15:54:15 +0300

    Updated package desctription

--- R/1-package.R
+++ R/1-package.R
@@ -1,22 +1,17 @@
-# TODO:
-#
-# 1) make "insert" and "replace" functions to insert text if there are
-#    several selections (see function rs_get_selection_text() parameter
-#    `selection` for details).
-
+# ============================================================================
 #' @name addin.tools
 #' @docType package
 #'
-#' @title Helper Functions to Create RStudio Add-ins
-#'
+#' @title Helper functions to create RStudio add-ins.
 #'
 #' @description
-#'
-#' Package \code{addin.tools} contains various functions that help to construct "RStudio" add-ins.
+#' Package \code{addin.tools} contains various functions that help to construct
+#' "RStudio" add-ins.
 #'
 #' \code{License:} MIT   \cr
-#' \code{URL:} \url{https://github.com/GegznaV/addin.tools} \cr
-#' \code{Bug reports and suggestions:} \url{https://github.com/GegznaV/addin.tools/issues} \cr
+#' \code{URL:} \url{https://GegznaV.github.io/addin.tools} \cr
+#' \code{Bug reports and suggestions:}
+#'      \url{https://github.com/GegznaV/addin.tools/issues} \cr
 #' \code{Author:} Vilmantas Gegzna
 #'
 #' @import rstudioapi
