46b9a851f64d0370b10288c7cfef4f72736f532a
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-31 15:54:26 +0300

    Create 1-TODO.R

---
+++ R/1-TODO.R
@@ -0,0 +1,19 @@
+# TODO:
+#
+# 1) Allow "insert" and "replace" functionality for several selections,
+#    including multirow selections.
+#    (NOTE: make "insert" and "replace" functions to insert text if there are
+#    several selections. See function rs_get_selection_text() parameter
+#    `selection` for details).
+#
+# 2) Add unit tests:
+#    a) create several contexts to be used in unit testing;
+#    b) create actual tests where possible.
+#
+# 3) Remove:
+#    a) ambiguous functions;
+#    b) functions that are not used in any package;
+#    c) unnecessary/ambiguous functionality (lines of code, parameters).
+#
+# 4) Check if deprecated functions still exist in other packages before removing
+#    them.
