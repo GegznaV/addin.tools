7d67ce9b5277cdc9f804a425aeb39ed8f18d42b3
Vilmantas Gėgžna <GegznaV@gmail.com>
2026-07-31 00:04:17 +0300

    List global variables

--- R/1-package.R
+++ R/1-package.R
@@ -18,3 +18,18 @@
 #' @importFrom utils globalVariables
 
 "_PACKAGE"
+
+utils::globalVariables(c(
+	"difference",
+	"end.column",
+	"end.row",
+	"end_diff",
+	"modify_group",
+	"new_end.column",
+	"new_start.column",
+	"rng",
+	"special",
+	"start.column",
+	"start.row",
+	"start_diff"
+))
