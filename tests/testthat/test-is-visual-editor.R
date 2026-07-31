9f6dcdbd7e0fc4e7a32795d67b2abdcfc4a20202
Vilmantas Gėgžna <GegznaV@gmail.com>
2026-07-31 08:41:11 +0300

    Update test-is-visual-editor.R

--- tests/testthat/test-is-visual-editor.R
+++ tests/testthat/test-is-visual-editor.R
@@ -13,5 +13,14 @@ test_that("is_visual_editor reflects empty/non-empty selection context", {
 })
 
 test_that("is_rmd_visual_mode is a backward-compatible alias", {
-  expect_identical(is_rmd_visual_mode, is_visual_editor)
+  testthat::local_mocked_bindings(
+    getActiveDocumentContext = function() list(selection = list(list(text = "x"))),
+    .package = "rstudioapi"
+  )
+
+  expect_warning(
+    expect_false(is_rmd_visual_mode()),
+    "deprecated",
+    fixed = FALSE
+  )
 })
