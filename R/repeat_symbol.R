bfb7ee2bcce5257bb1fe71c92013b85bb8c23699
Vilmantas Gėgžna <GegznaV@gmail.com>
2026-07-31 14:51:18 +0300

    Update repeat_symbol.R

--- R/repeat_symbol.R
+++ R/repeat_symbol.R
@@ -8,16 +8,14 @@
 #' @return Sting of defined length.
 #' @export
 #'
-#' @importFrom magrittr "%>%"
-#'
 #' @examples
 #'
 #' repeat_symbol(".", 10)
 #'
 #' repeat_symbol("..+", 10)
 repeat_symbol <- function(text, length.out) {
-  rep_len(text, length.out) %>%
-    paste0(collapse = "") %>%
+  rep_len(text, length.out) |>
+    paste0(collapse = "") |>
     substr(1, length.out)
 }
 
